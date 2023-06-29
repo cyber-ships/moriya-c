4603311d44503e8d5077319f47c0d925fd3b2fd4
梅田光雄 <mitsuo.u05@gmail.com>
2023-06-29 19:34:11 +0900

    company完成

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$(function () {
+  $(".company_client_accordion_btn").click(function () {
+    var $container = $(this).closest(".company_client_box");
+    var $open = $container.find(".company_client_accordion");
+
+    if ($open.hasClass("open")) {
+      $open.removeClass("open");
+      $open.slideUp();
+      $(this).text("∨");
+      $container.find(".company_client_accordion_text").fadeOut();
+    } else {
+      $open.addClass("open");
+      $open.slideDown();
+      $(this).text("∧");
+      $container.find(".company_client_accordion_text").fadeIn();
+    }
+  });
+});
