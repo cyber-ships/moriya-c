e1888b7ca7b0202982a2ee43b0f09497f35ada9c
梅田光雄 <mitsuo.u05@gmail.com>
2023-07-06 11:08:45 +0900

    ir作成

--- js/script.js
+++ js/script.js
@@ -15,4 +15,29 @@ $(function () {
       $container.find(".company_client_accordion_text").fadeIn();
     }
   });
+
+  $(".ir_others_faq_accordion_open").click(function () {
+    var $content = $(this).closest(".ir_others_faq_box_l");
+    var $answer = $content.find(".ir_others_faq_box_answer");
+
+    $(".ir_others_faq_box_answer").not($answer).slideUp();
+
+    $answer.slideToggle();
+  });
+
+  $(".ir_others_faq_accordion_close").click(function () {
+    var $content = $(this).closest(".ir_others_faq_box_l");
+    var $answer = $content.find(".ir_others_faq_box_answer");
+
+    $answer.slideUp();
+  });
 });
+/*if ($answer.hasClass("open")) {
+  $answer.removeClass("open");
+  $answer.slideUp();
+  $content.find(".company_client_accordion_text").fadeOut();
+} else {
+  $answer.addClass("open");
+  $answer.slideDown();
+  $content.find(".company_client_accordion_text").fadeIn();
+}*/
