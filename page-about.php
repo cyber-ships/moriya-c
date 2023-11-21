634312f57888c0632809e6a814689b930281d008
Shimoyama Naoki <92614673+naoki-shimoyma@users.noreply.github.com>
2023-11-21 16:01:04 +0900

    Update page-about.php

--- page-about.php
+++ page-about.php
@@ -2,200 +2,232 @@
 
     <main>
       <!-- company_page_heading -->
-      <section class="page_heading ir">
+      <section class="page_heading about">
         <div class="inner">
           <h1>
-            <span class="text"><small>Investors</small>IR情報</span>
+            <span class="text"><small>About us</small>私たちについて</span>
             <picture>
-              <img src="<?php echo esc_url(get_theme_file_uri('/img/ir/heading_img.jpg')); ?>" alt="製品紹介" />
+              <img src="<?php echo esc_url(get_theme_file_uri('/img/about/heading_img.png')); ?>" alt="企業情報" />
             </picture>
           </h1>
         </div>
       </section>
       <!-- company_page_heading -->
 
-      <!-- breadcrumbs -->
       <ul class="breadcrumbs">
-        <li><a href="../index.html">トップページ</a></li>
-        <li>IR情報</li>
+        <li><a href="#">トップページ</a></li>
+        <li>私たちについて</li>
       </ul>
-      <!-- /breadcrumbs -->
 
-      <!-- ir -->
-      <section>
+      <!-- about -->
+      <section class="about">
         <div class="inner">
-          <div class="ir_info">
-            <ul>
-              <li>
-                <dl>
-                  <dt>
-                    <span class="date">2023/02/14</span
-                    ><span class="cat">法定開示</span>
-                  </dt>
-                  <dd>
-                    <a class="pdf" href="#"
-                      >四半期報告書－第76期第3四半期(2022/10/01－2022/12/31)</a
-                    >
-                  </dd>
-                </dl>
-              </li>
-              <li>
-                <dl>
-                  <dt>
-                    <span class="date">2023/02/13</span
-                    ><span class="cat">決算短信</span>
-                  </dt>
-                  <dd>
-                    <a class="pdf" href="#"
-                      >2023年3月期　第3四半期短信[日本基準]（非連結）</a
-                    >
-                  </dd>
-                </dl>
-              </li>
-              <li>
-                <dl>
-                  <dt>
-                    <span class="date">2022/12/16</span
-                    ><span class="cat">IR資料</span>
-                  </dt>
-                  <dd>
-                    <a class="pdf" href="#"
-                      >会社説明会プレゼンテーションおよび質疑応答動画（2022年11月21日開催）</a
-                    >
-                  </dd>
-                </dl>
-              </li>
-            </ul>
-            <div class="txt_lnk">
-              <a href="../ir/news/index.html">IR NEWS一覧はこちら</a>
-            </div>
-          </div>
+          <h2 class="ttl_l">
+            エレベーターで、<br class="sp" />社会を支えます<br /><span
+              >Support the society with elevator
+            </span>
+          </h2>
+          <p class="lead">
+            私たち守谷輸送機工業は、<br
+              class="sp"
+            />荷物用エレベーターのパイオニアです。<br />
 
-          <div class="ir_cat_list">
-            <div class="block financial">
-              <a href="../ir/finance_info/index.html">
-                <div class="ttl">
-                  業績・財務情報<small>Financial Information</small>
-                </div>
-              </a>
-              <div class="list">
-                <ul>
-                  <li>
-                    <a href="../ir/finance_info/performance/index.html"
-                      >業績推移</a
-                    >
-                  </li>
-                  <li>
-                    <a href="../ir/finance_info/finance/index.html">財務状況</a>
-                  </li>
-                  <li>
-                    <a href="../ir/finance_info/cashflow/index.html"
-                      >キャッシュフロー</a
-                    >
-                  </li>
-                </ul>
+            エレベーターの開発から設計、製造、<br
+              class="sp"
+            />施工、保守、リニューアルまでを<br />
+            一貫した体制で築いてきた歴史あるメーカーです。<br />
+            エレベーターを通じて、物流を担い、<br
+              class="sp"
+            />物流は経済を支え、未来を作ります。
+          </p>
+        </div>
+      </section>
+
+      <section class="bg">
+        <div class="inner">
+          <h2 class="about_title">
+            守谷エレベーターが<br class="sp" />選ばれる<span class="big">6</span
+            >つの理由<br /><span class="about_title_sub">6 Reasons </span>
+          </h2>
+          <div class="about_number">
+            <a href="#reason_1" class="about_number_item">
+              <div class="about_number_item_title">1</div>
+              <div class="about_number_item_copy">
+                荷物用のオーダー型<br />エレベーターで<br /><span
+                  >業界最大手
+                </span>
               </div>
-            </div>
-          </div>
+              <img src="<?php echo esc_url(get_theme_file_uri('/img/about/arw.png')); ?>" alt="" />
+            </a>
 
-          <div class="ir_cat_list">
-            <div class="block financial">
-              <a href="../ir/library/index.html">
-                <div class="ttl">IRライブラリ<small>IR Library</small></div>
-              </a>
-              <div class="list">
-                <ul>
-                  <li>
-                    <a href="../ir/library/results/index.html">決算短信</a>
-                  </li>
-                  <li>
-                    <a href="../ir/library/presentation/index.html"
-                      >決算説明資料</a
-                    >
-                  </li>
-                  <li>
-                    <a href="../ir/library/disclosure/index.html"
-                      >適時開示資料</a
-                    >
-                  </li>
-                  <li>
-                    <a href="../ir/library/securities/index.html"
-                      >有価証券報告書等法定開示資料</a
-                    >
-                  </li>
-                  <li>
-                    <a href="../ir/library/others/index.html">IR資料</a>
-                  </li>
-                  <li>
-                    <a href="../ir/library/meeting/index.html"
-                      >株主総会関連資料</a
-                    >
-                  </li>
-                  <li>
-                    <a href="../ir/library/governance/index.html"
-                      >コーポレート・ガバナンス</a
-                    >
-                  </li>
-                </ul>
+            <a href="#reason_2" class="about_number_item">
+              <div class="about_number_item_title">2</div>
+              <div class="about_number_item_copy">
+                「ご希望の<br />エレベーター」に<br /><span
+                  >カスタマイズ可能</span
+                >
               </div>
-            </div>
-          </div>
+              <img src="<?php echo esc_url(get_theme_file_uri('/img/about/arw.png')); ?>" alt="" />
+            </a>
 
-          <div class="ir_cat_list">
-            <div class="block financial">
-              <a href="../ir/stock_info/index.html">
-                <div class="ttl">株式情報<small>Stock Information</small></div>
-              </a>
-              <div class="list">
-                <ul>
-                  <li>
-                    <a href="../ir/stock_info/stock/index.html">株式基本情報</a>
-                  </li>
-                  <li>
-                    <a href="../ir/stock_info/quote/index.html">株価情報</a>
-                  </li>
-                </ul>
+            <a href="#reason_3" class="about_number_item">
+              <div class="about_number_item_title">3</div>
+              <div class="about_number_item_copy">
+                老朽化した<br />エレベーターの<br /><span
+                  >リニューアル（入替）<br />にも対応</span
+                >
               </div>
-            </div>
-          </div>
+              <img src="<?php echo esc_url(get_theme_file_uri('/img/about/arw.png')); ?>" alt="" />
+            </a>
 
-          <div class="ir_cat_list">
-            <div class="block financial">
-              <a href="../ir/others/index.html">
-                <div class="ttl">
-                  その他情報<small>Other IR Information</small>
-                </div>
-              </a>
-              <div class="list">
-                <ul>
-                  <li><a href="../company/profile/index.html">会社概要</a></li>
-                  <li>
-                    <a href="../company/message/index.html">代表メッセージ</a>
-                  </li>
-                  <li>
-                    <a href="../ir/others/calender/index.html">IRカレンダー</a>
-                  </li>
-                  <li>
-                    <a href="../ir/others/policy/index.html"
-                      >ディスクロージャーポリシー</a
-                    >
-                  </li>
-                  <li>
-                    <a href="../ir/others/disclimer/index.html">免責事項</a>
-                  </li>
-                </ul>
+            <a href="#reason_4" class="about_number_item">
+              <div class="about_number_item_title">4</div>
+              <div class="about_number_item_copy">
+                オリジナル<br />垂直自動搬送機<br /><span
+                  >「マックリフター」</span
+                >
               </div>
-            </div>
-          </div>
+              <img src="<?php echo esc_url(get_theme_file_uri('/img/about/arw.png')); ?>" alt="" />
+            </a>
 
-          <div class="btn_wrap04">
-            <a class="btn type-01" href="../ir/notice/index.html"
-              ><span>電子広告</span></a
-            >
+            <a href="#reason_5" class="about_number_item">
+              <div class="about_number_item_title">5</div>
+              <div class="about_number_item_copy">
+                製造設置メーカー<br />ならではの<span>点検技術力</span>
+              </div>
+              <img src="<?php echo esc_url(get_theme_file_uri('/img/about/arw.png')); ?>" alt="" />
+            </a>
+
+            <a href="#reason_6" class="about_number_item">
+              <div class="about_number_item_title">6</div>
+              <div class="about_number_item_copy">
+                <span>全国52箇所</span>の<br />サービスステーション<br
+                  class="sp"
+                />による<br class="pc" /><span
+                  >24時間365日の<br />サービス対応</span
+                >
+              </div>
+              <img src="<?php echo esc_url(get_theme_file_uri('/img/about/arw.png')); ?>" alt="" />
+            </a>
           </div>
         </div>
       </section>
-      <!-- /ir -->
+
+      <section>
+        <div class="inner">
+          <div class="block_type_01">
+            <div id="reason_1" class="block anchor">
+              <div class="block-img">
+                <picture>
+                  <img src="<?php echo esc_url(get_theme_file_uri('/img/about/about_reason_1.jpg')); ?>" alt="" />
+                </picture>
+              </div>
+              <div class="block-cnt">
+                <div class="num">理由<em>1</em></div>
+                <h2 class="ttl">
+                  荷物用のオーダー型<br />エレベーターで<span>業界最大手</span>
+                </h2>
+                <p>
+                  長きにわたり、全国の様々な施設にて<br
+                    class="pc"
+                  />採用されてまいりました。
+                </p>
+              </div>
+            </div>
+
+            <div id="reason_2" class="block anchor">
+              <div class="block-img">
+                <picture>
+                  <img src="<?php echo esc_url(get_theme_file_uri('/img/about/about_reason_2.jpg')); ?>" alt="" />
+                </picture>
+              </div>
+              <div class="block-cnt">
+                <div class="num">理由<em>2</em></div>
+                <h2 class="ttl">
+                  「ご希望のエレベーター」<br /><span>カスタマイズ可能</span>
+                </h2>
+                <p>
+                  荷役、運ぶ荷物に合わせた製品づくり。ご使用方法や、環境に配慮した幅広いオプション群からのご提案、様々な意匠や表示、塗装、材質、プログラムに至るまで、お客様のニーズにお応えいたします。
+                </p>
+              </div>
+            </div>
+
+            <div id="reason_3" class="block anchor">
+              <div class="block-img">
+                <picture>
+                  <img src="<?php echo esc_url(get_theme_file_uri('/img/about/about_reason_3.jpg')); ?>" alt="" />
+                </picture>
+              </div>
+              <div class="block-cnt">
+                <div class="num">理由<em>3</em></div>
+                <h2 class="ttl">
+                  老朽化したエレベーターの<br /><span
+                    >リニューアル（入替）にも対応</span
+                  >
+                </h2>
+                <p>
+                  エレベーターは物流の要であり、また資産としても適切な維持・管理が重要なものとなります。他社製のエレベーターを含めたリニューアル（入替）にも対応しております。
+                </p>
+              </div>
+            </div>
+
+            <div id="reason_4" class="block anchor">
+              <div class="block-img">
+                <picture>
+                  <img src="<?php echo esc_url(get_theme_file_uri('/img/about/about_reason_4.jpg')); ?>" alt="" />
+                </picture>
+              </div>
+              <div class="block-cnt">
+                <div class="num">理由<em>4</em></div>
+                <h2 class="ttl">
+                  オリジナル垂直自動搬送期<br /><span>「マックリフター」</span>
+                </h2>
+                <p>
+                  エレベーター技術とコンベア技術の融合を図って開発した、弊社オリジナルのパレット専用垂直自動搬送機「マックリフター」。物流革命の旗手として、またエレベーターと同様、24時間365日のメンテナンス体制を併せ持つオンリーワンの設備として多方面で評価していただいております。
+                </p>
+              </div>
+            </div>
+
+            <div id="reason_5" class="block anchor">
+              <div class="block-img">
+                <picture>
+                  <img src="<?php echo esc_url(get_theme_file_uri('/img/about/about_reason_5.jpg')); ?>" alt="" />
+                </picture>
+              </div>
+              <div class="block-cnt">
+                <div class="num">理由<em>5</em></div>
+                <h2 class="ttl">
+                  製造メーカーならではの<br /><span>点検技術力</span>
+                </h2>
+                <p>
+                  創業以来約70年にわたって蓄積された技術とノウハウを持ったサービスマンによるメンテナンスを実施し、故障を未然に防ぎます。各サービスステーションには純正消耗部品を、本社、支店及びパーツセンターには重要部品を在庫し、緊急時には速やかな現場復旧を目指します。また、製作部品に関しても自社工場にて製造対応いたします。
+                </p>
+              </div>
+            </div>
+
+            <div id="reason_6" class="block anchor">
+              <div class="block-img">
+                <picture>
+                  <img src="<?php echo esc_url(get_theme_file_uri('/img/about/about_reason_6.jpg')); ?>" alt="" />
+                </picture>
+              </div>
+              <div class="block-cnt">
+                <div class="num">理由<em>6</em></div>
+                <h2 class="ttl">
+                  <span>全国52箇所の</span><br />
+                  サービスステーションによる<br /><span
+                    >24時間365日のサービス対応</span
+                  >
+                </h2>
+                <p>
+                  全国52ヶ所にサービスステーションを設置。細やかな保守と万が一の故障時には迅速な対応が可能です。故障時は365日24時間対応の本社サービス情報センターで受付、速やかにサービスステーションまたは巡回中のサービスマンを現地へ派遣いたします。
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!-- /about -->
     </main>
 
-  <?php get_footer(); ?>
+    <?php get_footer(); ?>
