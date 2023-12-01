9560a22980cda6a512722795c0a468978af56e41
梅田光雄 <mitsuo.u05@gmail.com>
2023-12-01 16:58:20 +0900

    12/1UM-修正

--- functions.php
+++ functions.php
@@ -26,127 +26,169 @@ function add_files() {
   wp_enqueue_style('modules-style', get_template_directory_uri() . '/assets/css/style.css');
 
   // Top Styles
-  wp_enqueue_style('top-style', get_template_directory_uri() . '/css/top.css');
+  if(is_front_page()){
+    wp_enqueue_style('top-style', get_template_directory_uri() . '/css/top.css');
+  }
 
   // maintenance Styles
-  wp_enqueue_style('maintenance-style', get_template_directory_uri() . '/css/maintenance.css'); 
+  if(is_page('maintenance')){
+    wp_enqueue_style('maintenance-style', get_template_directory_uri() . '/css/maintenance.css'); 
+  }
 
   // maintenance/partial Styles
-  wp_enqueue_style('maintenance_partial-style', get_template_directory_uri() . '/css/maintenance_partial.css');
+  if(is_page('maintenance_partial')){
+    wp_enqueue_style('maintenance_partial-style', get_template_directory_uri() . '/css/maintenance_partial.css');
+  }
 
   // about Styles
-  wp_enqueue_style('about-style', get_template_directory_uri() . '/css/about.css');
+  if(is_page('about')){
+    wp_enqueue_style('about-style', get_template_directory_uri() . '/css/about.css');
+  }
 
   // company Styles
-  wp_enqueue_style('company-style', get_template_directory_uri() . '/css/company.css');
+  if(is_page('company')){
+    wp_enqueue_style('company-style', get_template_directory_uri() . '/css/company.css');
+  }
 
   // company Styles/profile Styles
-  wp_enqueue_style('company_profile-style', get_template_directory_uri() . '/css/company_profile.css');
+  if(is_page('company_profile')){
+    wp_enqueue_style('company_profile-style', get_template_directory_uri() . '/css/company_profile.css');
+  }
 
   // company Styles/office Styles
-  wp_enqueue_style('company_office-style', get_template_directory_uri() . '/css/company_office.css');
+  if(is_page('company_office')){
+    wp_enqueue_style('company_office-style', get_template_directory_uri() . '/css/company_office.css');
+  }
 
   // company Styles/message Styles
-  wp_enqueue_style('company_message-style', get_template_directory_uri() . '/css/company_message.css');
+  if(is_page('company_message')){
+    wp_enqueue_style('company_message-style', get_template_directory_uri() . '/css/company_message.css');
+  }
 
   // company Styles/history Styles
-  wp_enqueue_style('company_history-style', get_template_directory_uri() . '/css/company_history.css');
+  if(is_page('company_history')){
+    wp_enqueue_style('company_history-style', get_template_directory_uri() . '/css/company_history.css');
+  }
 
   // company Styles/client Styles
-  wp_enqueue_style('company_client-style', get_template_directory_uri() . '/css/company_client.css');
+  if(is_page('company_client')){
+    wp_enqueue_style('company_client-style', get_template_directory_uri() . '/css/company_client.css');
+  }
 
   // ir Styles
-  wp_enqueue_style('ir-style', get_template_directory_uri() . '/css/ir.css');
+  if(is_page('ir')){
+    wp_enqueue_style('ir-style', get_template_directory_uri() . '/css/ir.css');
+  }
 
   // ir Styles/news Styles
-  wp_enqueue_style('ir_news-style', get_template_directory_uri() . '/css/ir_news.css');
+  if(is_post_type_archive('ir_news') || is_tax('ir_news_category','ir_cat-01') || is_tax('ir_news_category','ir_cat-02') || is_tax('ir_news_category','ir_cat-03') || is_tax('ir_news_category','ir_cat-04')){
+    wp_enqueue_style('ir_news-style', get_template_directory_uri() . '/css/ir_news.css');
+  }
 
   // ir Styles/news Styles/message Styles
-  wp_enqueue_style('ir_news_message-style', get_template_directory_uri() . '/css/ir_news_message.css');
+  if(is_page('ir_news_message')){
+    wp_enqueue_style('ir_news_message-style', get_template_directory_uri() . '/css/ir_news_message.css');
+  }
 
   // ir Styles/finance_info Styles
-  wp_enqueue_style('ir_finance_info-style', get_template_directory_uri() . '/css/ir_finance_info.css');
+  if(is_page('ir_finance_info')){
+    wp_enqueue_style('ir_finance_info-style', get_template_directory_uri() . '/css/ir_finance_info.css');
+  }
 
   // ir Styles/finance_info Styles/performance Styles
-  wp_enqueue_style('ir_finance_info_performance-style', get_template_directory_uri() . '/css/ir_finance_info_performance.css');
+  if(is_page('ir_finance_info_performance')){
+    wp_enqueue_style('ir_finance_info_performance-style', get_template_directory_uri() . '/css/ir_finance_info_performance.css');
+  }
 
   // ir Styles/finance_info Styles/finance Styles
-  wp_enqueue_style('ir_finance_info_finance-style', get_template_directory_uri() . '/css/ir_finance_info_finance.css');
+  if(is_page('ir_finance_info_finance')){
+    wp_enqueue_style('ir_finance_info_finance-style', get_template_directory_uri() . '/css/ir_finance_info_finance.css');
+  }
 
   // ir Styles/finance_info Styles/cashflow Styles
-  wp_enqueue_style('ir_finance_info_cashflow-style', get_template_directory_uri() . '/css/ir_finance_info_cashflow.css');
+  if(is_page('ir_finance_info_cashflow')){
+    wp_enqueue_style('ir_finance_info_cashflow-style', get_template_directory_uri() . '/css/ir_finance_info_cashflow.css');
+  }
 
   // ir Styles/library Styles
-  wp_enqueue_style('ir_library-style', get_template_directory_uri() . '/css/ir_library.css');
+  if(is_post_type_archive('ir_library')){
+    wp_enqueue_style('ir_library-style', get_template_directory_uri() . '/css/ir_library.css');
+  }
 
   // ir Styles/library Styles/results Styles
-  wp_enqueue_style('ir_library_results-style', get_template_directory_uri() . '/css/ir_library_results.css');
+  if(is_tax('ir_library_category','library_cat-01')){
+    wp_enqueue_style('ir_library_results-style', get_template_directory_uri() . '/css/ir_library_results.css');
+  }
 
   // ir Styles/library Styles/presentations Styles
-  wp_enqueue_style('ir_library_presentations-style', get_template_directory_uri() . '/css/ir_library_presentation.css');
+  if(is_tax('ir_library_category','library_cat-02')){
+    wp_enqueue_style('ir_library_presentations-style', get_template_directory_uri() . '/css/ir_library_presentation.css');
+  }
 
   // ir Styles/library Styles/securities Styles
-  wp_enqueue_style('ir_library_securities-style', get_template_directory_uri() . '/css/ir_library_securities.css');
+  if(is_tax('ir_library_category','library_cat-04')){
+    wp_enqueue_style('ir_library_securities-style', get_template_directory_uri() . '/css/ir_library_securities.css');
+  }
 
   // ir Styles/library Styles/meeting Styles
-  wp_enqueue_style('ir_library_meeting-style', get_template_directory_uri() . '/css/ir_library_meeting.css');
+  if(is_tax('ir_library_category','library_cat-06')){
+    wp_enqueue_style('ir_library_meeting-style', get_template_directory_uri() . '/css/ir_library_meeting.css');
+  }
 
   // ir Styles/library Styles/others Styles
-  wp_enqueue_style('ir_library_others-style', get_template_directory_uri() . '/css/ir_library_others.css');
-  
+  if(is_tax('ir_library_category','library_cat-05')){
+    wp_enqueue_style('ir_library_others-style', get_template_directory_uri() . '/css/ir_library_others.css');
+  }
+
   // ir Styles/library Styles/disclosure Styles
-  wp_enqueue_style('ir_library_disclosure-style', get_template_directory_uri() . '/css/ir_library_disclosure.css');
+  if(is_tax('ir_library_category','library_cat-03')){
+    wp_enqueue_style('ir_library_disclosure-style', get_template_directory_uri() . '/css/ir_library_disclosure.css');
+  }
 
   // ir Styles/library Styles/governance Styles
-  wp_enqueue_style('ir_library_governance-style', get_template_directory_uri() . '/css/ir_library_governance.css');
-
+  if(is_tax('ir_library_category','library_cat-07')){
+    wp_enqueue_style('ir_library_governance-style', get_template_directory_uri() . '/css/ir_library_governance.css');
+  }
+  
   // ir Styles/stock_info Styles
-  wp_enqueue_style('ir_stock_info-style', get_template_directory_uri() . '/css/ir_stock_info.css');
-
-  // ir Styles/stock_info Styles/stock Styles
-  wp_enqueue_style('ir_stock_info_stock-style', get_template_directory_uri() . '/css/ir_stock_info.css');
-
-  // ir Styles/stock_info Styles/quote Styles
-  wp_enqueue_style('ir_stock_info_quote-style', get_template_directory_uri() . '/css/ir_stock_info.css');
+  if(is_page('ir_stock_info') || is_page('ir_stock_info_stock') || is_page('ir_stock_info_quote')){
+    wp_enqueue_style('ir_stock_info-style', get_template_directory_uri() . '/css/ir_stock_info.css');
+  }
 
   // ir Styles/others Styles
-  wp_enqueue_style('ir_others-style', get_template_directory_uri() . '/css/ir_others.css');
-
-  // ir Styles/others Styles/calender Styles
-  wp_enqueue_style('ir_others_calender-style', get_template_directory_uri() . '/css/ir_others.css');
-
-  // ir Styles/others Styles/policy Styles
-  wp_enqueue_style('ir_others_policy-style', get_template_directory_uri() . '/css/ir_others.css');
-
-  // ir Styles/others Styles/disclaimer Styles
-  wp_enqueue_style('ir_others_disclaimer-style', get_template_directory_uri() . '/css/ir_others.css');
+  if(is_page('ir_others') || is_page('ir_others_calender') || is_page('ir_others_policy') || is_page('ir_others_disclaimer')){
+    wp_enqueue_style('ir_others-style', get_template_directory_uri() . '/css/ir_others.css');
+  }
 
   // ir Styles/notice Styles
-  wp_enqueue_style('ir_notice-style', get_template_directory_uri() . '/css/ir_notice.css');
+  if(is_page('ir_notice')){
+    wp_enqueue_style('ir_notice-style', get_template_directory_uri() . '/css/ir_notice.css');
+  }
 
   // recruit Styles
-  wp_enqueue_style('recruit-style', get_template_directory_uri() . '/css/recruit.css');
+  if(is_page('recruit')){
+    wp_enqueue_style('recruit-style', get_template_directory_uri() . '/css/recruit.css');
+  }
 
   // recruit Styles/youkou Styles
-  wp_enqueue_style('recruit_youkou-style', get_template_directory_uri() . '/css/recruit_youkou.css');
-
-  // recruit Styles/youkou2 Styles
-  wp_enqueue_style('recruit_youkou2-style', get_template_directory_uri() . '/css/recruit_youkou.css');
-
-  // recruit Styles/message Styles
-  wp_enqueue_style('recruit_message-style', get_template_directory_uri() . '/css/recruit_youkou.css');
-
-  // recruit Styles/joblist Styles
-  wp_enqueue_style('recruit_joblist-style', get_template_directory_uri() . '/css/recruit_youkou.css');
+  if(is_page('recruit_youkou') || is_page('recruit_youkou2') || is_page('recruit_message') || is_page('recruit_joblist')){
+    wp_enqueue_style('recruit_youkou-style', get_template_directory_uri() . '/css/recruit_youkou.css');
+  }
 
   // recruit Styles/work Styles
-  wp_enqueue_style('recruit_work-style', get_template_directory_uri() . '/css/recruit_work.css');
-
+  if(is_post_type_archive('recruit_work') || is_tax('recruit_works_category','recruit_cat-01') || is_tax('recruit_works_category','recruit_cat-02') || is_tax('recruit_works_category','recruit_cat-03')
+  || is_tax('recruit_works_category','recruit_cat-04') || is_tax('recruit_works_category','recruit_cat-05') || is_tax('recruit_works_category','recruit_cat-06')){
+    wp_enqueue_style('recruit_work-style', get_template_directory_uri() . '/css/recruit_work.css');
+  }
   // recruit Styles/number Styles
-  wp_enqueue_style('recruit_number-style', get_template_directory_uri() . '/css/recruit_number.css');
+  if(is_page('recruit_number')){
+    wp_enqueue_style('recruit_number-style', get_template_directory_uri() . '/css/recruit_number.css');
+  }
 
   // recruit Styles/faq Styles
-  wp_enqueue_style('recruit_faq-style', get_template_directory_uri() . '/css/recruit_faq.css');
+  if(is_page('recruit_faq')){
+    wp_enqueue_style('recruit_faq-style', get_template_directory_uri() . '/css/recruit_faq.css');
+  }
 
   // products Styles
   wp_enqueue_style('products-style', get_template_directory_uri() . '/css/products.css');
@@ -155,11 +197,16 @@ function add_files() {
   wp_enqueue_style('products_renewal-style', get_template_directory_uri() . '/css/products_renewal.css');
 
   // policy Styles
-  wp_enqueue_style('policy-style', get_template_directory_uri() . '/css/policy.css');
+  if(is_page('policy') || is_page('policy_privacy') || is_page('policy_antisocial') || is_page('policy_security') || 
+  is_page('policy_gender') || is_page('policy_career')){
+    wp_enqueue_style('policy-style', get_template_directory_uri() . '/css/policy.css');
+  }
 
   // map Styles
-  wp_enqueue_style('map-style', get_template_directory_uri() . '/css/map.css');
-
+  if(is_page('map')){
+    wp_enqueue_style('map-style', get_template_directory_uri() . '/css/map.css');
+  }
+  
   // works Styles
   wp_enqueue_style('works-style', get_template_directory_uri() . '/css/works.css');
 
